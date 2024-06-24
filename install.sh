3a501c086426c4a0c2110601a2f876b0a7c1d141
Haoqiu Song <106828678+haoqiusong@users.noreply.github.com>
2024-06-24 10:06:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,3 +14,11 @@ rm diamond-linux64.tar.gz
 wget https://github.com/hyattpd/Prodigal/releases/download/v2.6.3/prodigal.linux
 mv prodigal.linux prodigal
 chmod +x prodigal
+
+# themisto installation
+wget https://github.com/algbio/themisto/releases/download/v3.2.2/themisto_linux-v3.2.2.tar.gz
+tar xzf themisto_linux-v3.2.2.tar.gz
+mv ./themisto_linux-v3.2.2/themisto ./
+chmod +x themisto
+rm themisto_linux-v3.2.2.tar.gz
+rm -r ./themisto_linux-v3.2.2
